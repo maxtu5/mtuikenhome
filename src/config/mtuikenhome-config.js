ceb9b97f447bdf3601014c2183bc64d6070edf57
m.tuikin <maximtu.all@gmail.com>
2025-12-17 17:45:31 +0200

    fix 8

--- src/config/mtuikenhome-config.js
+++ src/config/mtuikenhome-config.js
@@ -1,5 +1,5 @@
 const config = {
-    jsonPath: "data/mtuikenhome-content.json",
+    jsonPath: "/data/mtuikenhome-content.json",
     resumeLink: "https://drive.google.com/file/d/1WJFF1c0ackKmVKdD0uBGtDQf02ngf4ix/view"
 };
 
